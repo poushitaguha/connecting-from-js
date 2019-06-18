8538490e1a5c44a5ec6f099c97fe9f9ad0871c23
poushitaguha <pguha.kolkata@gmail.com>
2019-06-18 15:22:42 +0000

    Create two separate files and add code for JS assignment

--- test_script.js
+++ test_script.js
@@ -1,8 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
-const person = process.argv[2];
-
 const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
@@ -16,25 +14,11 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  // client.query("SELECT $1::int AS number", ["1"], (err, result) => {
-  client.query("SELECT * FROM famous_people WHERE first_name = $1", [person], (err, result) => {
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-
-    console.log("Searching...");
-    console.log("Found " + result.rows.length + " person(s) by the name " + "'" + person + "':");
-    let count = 0;
-    result.rows.forEach(person => {
-        count += 1;
-        let firstName = person.first_name;
-        let lastName = person.last_name;
-        let birthdate = person.birthdate;
-        birthdate = birthdate.toISOString().slice(0,10)
-        console.log("- " + count + ": ", firstName, lastName + ", born " + "'" + birthdate + "'");
-    });
-
-    // console.log(result.rows[0].number); //output: 1
+    console.log(result.rows[0].number); //output: 1
     client.end();
   });
 });
